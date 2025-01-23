82c7bcb949cb218b6d7e549458f10ddfb17db47f
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-01-24 01:27:01 +0800

    3

--- setup.php
+++ setup.php
@@ -4,7 +4,7 @@
         $db_host = 'localhost';
         $db_username = 'root';
         $db_password = '';
-        $db_name = 'hentoki_db';
+        $db_name = 'SantosOpticals';
 
         $conn = new mysqli($db_host, $db_username, $db_password, $db_name);
 
@@ -14,4 +14,47 @@
 
         return $conn;
     }
+    function createDB()
+    {
+        // Configuration
+        $db_host = 'localhost';
+        $db_username = 'root';
+        $db_password = '';
+
+        // Create connection
+        $conn = new mysqli($db_host, $db_username, $db_password);
+
+        // Check connection
+        if ($conn->connect_error) 
+        {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Creating database
+        $sql = "CREATE DATABASE SantosOpticals";
+
+        if ($conn->query($sql) === TRUE) 
+        {
+        } 
+        else 
+        {
+            echo "There is an error in creating the database: " . $conn->error;
+        }
+
+        $conn->close();
+    }
+?>
+
+<?php
+     $conn = new mysqli('localhost','root','');
+     $db_check_query = "SHOW DATABASES LIKE 'SantosOpticals'";
+ 
+     $result = mysqli_query($conn, $db_check_query);
+
+     if (mysqli_num_rows($result) == 0) 
+    {
+        createDB();
+        $conn->close();        
+    }
+
 ?>
