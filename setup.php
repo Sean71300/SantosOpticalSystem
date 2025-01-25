d072ef1ec311182c35ca01716b0fd4b385edc1c9
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-01-25 13:53:23 +0800

    01/25/2025
    Database and tables

--- setup.php
+++ setup.php
@@ -1,4 +1,7 @@
 <?php
+
+    // Connection
+
     function connect() 
     {
         $db_host = 'localhost';
@@ -14,6 +17,9 @@
 
         return $conn;
     }
+
+    // Create database
+
     function createDB()
     {
         // Configuration
@@ -43,6 +49,80 @@
 
         $conn->close();
     }
+    
+    // Create Employee Table
+
+    function create_EmployeesTable()
+    {
+        $conn = connect();
+
+        $sql = "CREATE TABLE employee(
+                EmployeeID INT(10) PRIMARY KEY,
+                EmployeeName VARCHAR(100),
+                EmployeePicture LONGBLOB,
+                EmployeeEmail VARCHAR(100),
+                EmployeeNumber VARCHAR(11),
+                RoleID INT(10),
+                LoginName VARCHAR(50),
+                Password VARCHAR(255),
+                BranchCode int(10),
+                Status VARCHAR(50),
+                Upd_by VARCHAR(50),
+                Upd_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )";
+
+        if (mysqli_query($conn, $sql))
+        {
+            $img_path = "Images/default.jpg";
+            $img_clean= file_get_contents($img_path);
+            $employee_pic = mysqli_real_escape_string($conn, $img_clean);
+            $id = generate_EmployeeID();
+            $password = "JPSantos123";
+            $email = "BVPSantosOptical@gmail.com";
+
+            $hashed_pw = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO employee
+                    (EmployeeID, EmployeeName, EmployeePicture,EmployeeEmail,
+                    EmployeeNumber,RoleID,LoginName,Password,BranchCode,Status,
+                    Upd_by)
+                    VALUES
+                    ($id, 'Bien Ven P. Santos', '$employee_pic', '$email', 
+                    '09864571325', '1', 'BVSantos1', '$hashed_pw', '1', 'Active',
+                     'Admin')";
+
+            mysqli_query($conn, $sql);
+        }
+        else
+        {
+            echo "<br>There is an error in creating the table: " . $conn->connect_error;
+        }
+
+        $conn->close();
+    }
+
+    //Generate Employee ID
+
+    function generate_EmployeeID()
+    {
+        $conn = connect();
+
+        $query = "SELECT COUNT(*) as count FROM employee";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        $rowCount = $row["count"];
+
+        // Get the current year
+        $currentYear = date("Y");
+
+        // Generate the ID
+        $genID = (int)($currentYear . str_pad(5, 2, "0", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
+        
+        $checkQuery = "SELECT EmployeeID FROM employee WHERE EmployeeID = ?";
+        $genID = checkDuplication($genID,$checkQuery);
+        $conn->close();
+        return $genID;
+    }
 ?>
 
 <?php
@@ -50,11 +130,19 @@
      $db_check_query = "SHOW DATABASES LIKE 'SantosOpticals'";
  
      $result = mysqli_query($conn, $db_check_query);
-
+    // Check if the database exists
      if (mysqli_num_rows($result) == 0) 
     {
         createDB();
         $conn->close();        
+    }
+    // Check if employee table exists
+    $table_check_query = "SHOW TABLES LIKE 'employee'";
+    $result = mysqli_query($conn, $table_check_query);
+
+    if (mysqli_num_rows($result) == 0) 
+    {
+        create_EmployeesTable();
     }
 
 ?>
