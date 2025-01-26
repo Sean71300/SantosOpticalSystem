91dd40fac97bb3b3125fde4cec5c482078e76d36
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-01-26 18:22:16 +0800

    01/26/2025
    Tables in products and product branch master added

--- setup.php
+++ setup.php
@@ -49,7 +49,270 @@
 
         $conn->close();
     }
-    
+
+    // Create Product Branch Master Table
+
+    function create_ProductBrnchMstrTable()
+    {
+        $conn = connect();
+        $sql = "CREATE TABLE ProductBranchMaster (
+                ProductBranchID INT(10) PRIMARY KEY,
+                ProductID INT(10),
+                BranchCode INT(10),
+                Count INT(100),
+                Avail_FL VARCHAR(50), 
+                Upd_by VARCHAR(50),
+                Upd_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )";
+
+        if (mysqli_query($conn, $sql))
+        {
+            
+            $id = generate_ProductBrnchMstrID();            
+            $sql = "INSERT INTO ProductBranchMaster
+                    (ProductBranchID,ProductID,BranchCode,Count,Avail_FL,Upd_by)
+                    VALUES
+                    ('$id', '2025140000', '1', '5', 'Available',
+                    'Bien Ven P. Santos'
+                    )";
+
+            mysqli_query($conn, $sql);
+        }
+        else
+        {
+            echo "<br>There is an error in creating the table: " . $conn->connect_error;
+        }
+
+        $conn->close();
+    }
+
+    // Generate Brand ID
+
+    function generate_ProductBrnchMstrID()
+    {
+        $conn = connect();
+
+        $query = "SELECT COUNT(*) as count FROM ProductBranchMaster";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        $rowCount = $row["count"];
+
+        // Get the current day
+        $currentYear = date("Y");
+
+        // Generate the ID
+        $genID = (int)($currentYear . str_pad(5, 2, "1", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
+        
+        $checkQuery = "SELECT ProductBranchID FROM ProductBranchMaster WHERE ProductBranchID = ?";
+        $genID = checkDuplication($genID,$checkQuery);
+        $conn->close();
+        return $genID;
+    }
+
+
+    // Create Category Type Table
+
+    function create_BrandMasterTable()
+    {
+        $conn = connect();
+
+        $sql = "CREATE TABLE brandMaster (
+                BrandID INT(10),
+                BrandName VARCHAR(500)
+                )";
+       
+        if (mysqli_query($conn, $sql))
+        {            
+            $genID = generate_BrandID();
+            $sql = "INSERT INTO brandMaster
+                    (BrandID,BrandName)
+                    VALUES
+                    ('$genID','Adensco'
+                    )";
+
+            mysqli_query($conn, $sql);
+        }
+        else
+        {
+            echo "<br>There is an error in creating the table: " . $conn->connect_error;
+        }
+
+        $conn->close();
+    }
+
+    // Generate Brand ID
+
+    function generate_BrandID()
+    {
+        $conn = connect();
+
+        $query = "SELECT COUNT(*) as count FROM brandMaster";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        $rowCount = $row["count"];
+
+        // Get the current day
+        $currentYear = date("Y");
+
+        // Generate the ID
+        $genID = (int)($currentYear . str_pad(5, 2, "1", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
+        
+        $checkQuery = "SELECT BrandID FROM brandMaster WHERE BrandID = ?";
+        $genID = checkDuplication($genID,$checkQuery);
+        $conn->close();
+        return $genID;
+    }
+
+    // Create Shape Master Table
+
+    function create_ShapeMasterTable()
+    {
+        $conn = connect();
+
+        $sql = "CREATE TABLE shapeMaster (
+                ShapeID INT(1),
+                Description VARCHAR(30)
+                )";
+       
+        if (mysqli_query($conn, $sql))
+        {            
+            $i = 0;
+            do {                
+                $i++;                       
+                switch ($i) {
+                    case 1:                        
+                        $shape = 'Oval';
+                        break;
+                    case 2:                        
+                        $shape = 'Triangle';
+                        break;
+                    case 3:                        
+                        $shape = 'Diamond';
+                        break;
+                    case 4:                        
+                        $shape = 'Round';
+                        break;    
+                    case 5:                        
+                        $shape = 'Square';
+                        break; 
+                    case 6:                        
+                        $shape = 'Square';
+                        break; 
+                    default:                    
+                    break;                
+                }
+                $sql = "INSERT INTO shapeMaster
+                (ShapeID,Description)
+                VALUES
+                ('$i','$shape')";
+                mysqli_query($conn, $sql);
+            } while ($i < 6);
+        }
+        else
+        {
+            echo "<br>There is an error in creating the table: " . $conn->connect_error;
+        }
+
+        $conn->close();
+    }
+
+    // Create Category Type Table
+
+    function create_CategoryTypeTable()
+    {
+        $conn = connect();
+
+        $sql = "CREATE TABLE categoryType (
+                CategoryType VARCHAR(50),
+                Description VARCHAR(500)
+                )";
+        $desc="Frames that will be used for the 
+                    customer\'s Glasses";
+        if (mysqli_query($conn, $sql))
+        {            
+            $sql = "INSERT INTO categoryType
+                    (CategoryType,Description)
+                    VALUES
+                    ('Frame','$desc'
+                    )";
+
+            mysqli_query($conn, $sql);
+        }
+        else
+        {
+            echo "<br>There is an error in creating the table: " . $conn->connect_error;
+        }
+
+        $conn->close();
+    }
+
+    // Create Product Master Table
+
+    function create_ProductMstrTable()
+    {
+        $conn = connect();
+        $sql = "CREATE TABLE productMstr (
+                ProductID INT(10) PRIMARY KEY,
+                CategoryType VARCHAR(50),
+                ShapeID INT(1),
+                BrandID INT(10),
+                Model VARCHAR(50),
+                Remarks VARCHAR(500),
+                ProductImage LONGBLOB,
+                Avail_FL VARCHAR (50),  
+                Upd_by VARCHAR(50),
+                Upd_dt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                )";
+
+        if (mysqli_query($conn, $sql))
+        {
+            $img_path = "Images/Frame1.jpg";
+            $img_clean= file_get_contents($img_path);
+            $product_pic = mysqli_real_escape_string($conn, $img_clean);
+            $id = generate_ProductMstrID();
+            
+            $sql = "INSERT INTO productMstr
+                    (ProductID,CategoryType,ShapeID,BrandID,Model,Remarks,
+                    ProductImage,Avail_FL,Upd_by)
+                    VALUES
+                    ('$id', 'Frame', '1', '2025070011', 
+                    'Model1', 'New Model', '$product_pic', 'Available', 
+                    'Bien Ven P. Santos'
+                    )";
+
+            mysqli_query($conn, $sql);
+        }
+        else
+        {
+            echo "<br>There is an error in creating the table: " . $conn->connect_error;
+        }
+
+        $conn->close();
+    }
+
+    //Generate Product Master ID
+
+    function generate_ProductMstrID()
+    {
+        $conn = connect();
+
+        $query = "SELECT COUNT(*) as count FROM productMstr";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        $rowCount = $row["count"];
+
+        // Get the current day
+        $currentYear = date("Y");
+
+        // Generate the ID
+        $genID = (int)($currentYear . str_pad(4, 2, "1", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
+        
+        $checkQuery = "SELECT ProductID FROM productMstr WHERE ProductID = ?";
+        $genID = checkDuplication($genID,$checkQuery);
+        $conn->close();
+        return $genID;
+    }
+
     // Create Employee Table
 
     function create_EmployeesTable()
@@ -101,6 +364,29 @@
         $conn->close();
     }
 
+    //Generate Employee ID
+
+    function generate_EmployeeID()
+    {
+        $conn = connect();
+
+        $query = "SELECT COUNT(*) as count FROM employee";
+        $result = $conn->query($query);
+        $row = $result->fetch_assoc();
+        $rowCount = $row["count"];
+
+        // Get the current year
+        $currentYear = date("Y");
+
+        // Generate the ID
+        $genID = (int)($currentYear . str_pad(3, 2, "1", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
+        
+        $checkQuery = "SELECT EmployeeID FROM employee WHERE EmployeeID = ?";
+        $genID = checkDuplication($genID,$checkQuery);
+        $conn->close();
+        return $genID;
+    }
+    
     //Create Customer Table
 
     function create_CustomersTable()
@@ -140,29 +426,6 @@
         $conn->close();
     }
 
-    //Generate Employee ID
-
-    function generate_EmployeeID()
-    {
-        $conn = connect();
-
-        $query = "SELECT COUNT(*) as count FROM employee";
-        $result = $conn->query($query);
-        $row = $result->fetch_assoc();
-        $rowCount = $row["count"];
-
-        // Get the current year
-        $currentYear = date("Y");
-
-        // Generate the ID
-        $genID = (int)($currentYear . str_pad(5, 2, "0", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
-        
-        $checkQuery = "SELECT EmployeeID FROM employee WHERE EmployeeID = ?";
-        $genID = checkDuplication($genID,$checkQuery);
-        $conn->close();
-        return $genID;
-    }
-
     //Generate Customer ID
 
     function generate_CustomerID()
@@ -176,10 +439,10 @@
 
         // Get the current year
         $currentYear = date("Y");
-
-        // Generate the ID
-        $genID = (int)($currentYear . str_pad(5, 2, "0", STR_PAD_LEFT) . str_pad($rowCount, 4, "0", STR_PAD_LEFT));
+        $currentMonth = date("m");
         
+        // Generate the ID
+        $genID = (int)($currentYear . $currentMonth . str_pad($rowCount, 4, "0", STR_PAD_LEFT));        
         $checkQuery = "SELECT CustomerID FROM customer WHERE CustomerID = ?";
         $genID = checkDuplication($genID,$checkQuery);
         $conn->close();
@@ -242,4 +505,47 @@
     {
         create_CustomersTable();
     }
+
+    // Check if Product Master Table exists
+    $table_check_query = "SHOW TABLES LIKE 'productMstr'";
+    $result = mysqli_query($conn, $table_check_query);
+
+    if (mysqli_num_rows($result) == 0) 
+    {
+        create_ProductMstrTable();
+    }
+
+    // Check if Product Master Table exists
+    $table_check_query = "SHOW TABLES LIKE 'CategoryType'";
+    $result = mysqli_query($conn, $table_check_query);
+
+    if (mysqli_num_rows($result) == 0) 
+    {
+        create_CategoryTypeTable();
+    }
+    // Check if Shape Master Table exists
+    $table_check_query = "SHOW TABLES LIKE 'shapeMaster'";
+    $result = mysqli_query($conn, $table_check_query);
+
+    if (mysqli_num_rows($result) == 0) 
+    {
+        create_ShapeMasterTable();
+    }
+    // Check if Shape Master Table exists
+    $table_check_query = "SHOW TABLES LIKE 'brandMaster'";
+    $result = mysqli_query($conn, $table_check_query);
+
+    if (mysqli_num_rows($result) == 0) 
+    {
+        create_BrandMasterTable();
+    }
+    // Check if Product Branch Master Table exists
+    $table_check_query = "SHOW TABLES LIKE 'ProductBranchMaster'";
+    $result = mysqli_query($conn, $table_check_query);
+
+    if (mysqli_num_rows($result) == 0) 
+    {
+        create_ProductBrnchMstrTable();
+    }
+    
 ?>
