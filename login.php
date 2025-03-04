d27a6b0ef45f81945fff0f7e1bac6d4369ca0b8e
esistlex <estislex@gmail.com>
2025-03-04 21:19:56 +0800

    implement admin [waiting for foreign keys 🤔]

--- login.php
+++ login.php
@@ -49,9 +49,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                             if($roleid == 1) {
                                 header("location: admin.php");
                             } else if($roleid == 2) {
-                                header("location: welcome.php");
-                            } else {
-                            header("location: welcome.php");
+                                header("location: employee.php");
                             }
                         } else {
                             $login_err = "The password you entered was not valid.";
