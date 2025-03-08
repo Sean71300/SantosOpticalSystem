32353467be113592731dbb463fb4ac2287df33e6
esistlex <estislex@gmail.com>
2025-03-09 03:44:40 +0800

    ayos kaunti sa pdmain

--- pdmain.php
+++ pdmain.php
@@ -16,8 +16,9 @@ include_once 'setup.php';
 
     <nav class="navbar navbar-expand-lg bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="demo1.html">
-                <img src="logo.png" alt="Logo"> BVP Santos Optical
+            <a class="navbar-brand fw-bold" href="demo1.html">
+                <img src="images/logo.png" alt="Logo" width="64" height="64"> 
+                BVP Santos Optical
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
