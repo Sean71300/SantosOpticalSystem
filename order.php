3ad1c1a2a8c904ff990a38cc2621ca70e6f26c83
esistlex <estislex@gmail.com>
2025-04-25 18:13:24 +0800

    NAVIGATION - added order.php ORDER - yipee

---
+++ order.php
@@ -0,0 +1,28 @@
+<?php
+include 'ActivityTracker.php';
+include 'loginChecker.php';
+include 'admin-inventory-funcs.php'; // Include the functions file
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+        <link rel="stylesheet" href="customCodes/custom.css">
+        <title>Order</title>
+        <link rel="shortcut icon" type="image/x-icon" href="images/logo.png"/>
+    </head>
+
+    <header class="mb-5">
+        <?php
+            include "Navigation.php";
+        ?>
+    </header>
+
+    <body>
+        <div class="container">
+            THE FUN BEGINS
+        </div>
+    </body>
+</html>
