f6a051f38b638d5f975615cbb9c0d587853acb46
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-04-28 15:12:18 +0800

    04/28/2025

--- admin.php
+++ admin.php
@@ -88,46 +88,7 @@ include 'loginChecker.php';
     </head>
 
     <body>
-        <!-- Sidebar -->
-        <div class="sidebar">
-            <div class="sidebar-header">
-                <h4><i class="fas fa-cog"></i> Admin Panel</h4>
-            </div>
-            
-            <div class="sidebar-menu">
-                <a href="#" class="sidebar-item active">
-                    <i class="fas fa-tachometer-alt"></i> Dashboard
-                </a>
-                
-                <a href="customerRecords.php" class="sidebar-item">
-                    <i class="fas fa-users"></i> Customer Information
-                </a>
-                
-                <a href="EmployeeRecords.php" class="sidebar-item">
-                    <i class="fas fa-user-tie"></i> Manage Employees
-                </a>
-                
-                <a href="admin-inventory.php" class="sidebar-item">
-                    <i class="fas fa-boxes"></i> Manage Inventory
-                </a>
-                
-                <a href="#" class="sidebar-item">
-                    <i class="fas fa-shopping-cart"></i> Orders
-                </a>
-                
-                <a href="#" class="sidebar-item">
-                    <i class="fas fa-box-open"></i> Products
-                </a>
-                
-                <a href="#" class="sidebar-item">
-                    <i class="fas fa-layer-group"></i> Collections
-                </a>
-                
-                <a href="#" class="sidebar-item">
-                    <i class="fas fa-archive"></i> Package
-                </a>
-            </div>
-        </div>
+        <?php include "employeeSidebar.php"?>
 
         <!-- Main Content -->
         <div class="main-content">
--- employeeSidebar.php
+++ employeeSidebar.php
@@ -1,93 +1,39 @@
-<div class="forNavigationbar sticky-start">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary flex-column align-items-start h-100">
-        <div class="container-fluid flex-column h-100 d-flex justify-content-between">
-            <div class="w-100">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse w-100" id="navbarSupportedContent">
-                    <ul class="navbar-nav flex-column w-100 fs-5 fw-bold">                  
-                        <li class="nav-item w-100">
-                            <a class="nav-link my-2" href="order.php">ORDER</a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a class="nav-link my-2" href="product-gallery.php">PRODUCTS</a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a class="nav-link my-2" href="#">COLLECTIONS</a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a class="nav-link my-2" href="#">PACKAGE</a>
-                        </li>
-                        <li class="nav-item w-100">
-                            <a class="nav-link my-2" href="aboutus.php">ABOUT</a>
-                        </li>                                  
-                        <?php  
-                        if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-                            echo '<li class="nav-item w-100 my-2">';
-                            echo '<a class="nav-link" href="Login.php">Login</a>';
-                            echo '</li>';
-                        }
-                        else{     
-                        $image = $_SESSION["img"];
-                        echo  '<div class="dropdown w-100 my-2">';
-                        echo  '<button class="btn dropdown-toggle fs-5 fw-bold w-100 text-start" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">';
-                        echo  htmlspecialchars($_SESSION["full_name"]) . '  ';   
-                        echo  '<img src="' . $image . '"class="logo">';
-                        echo  '</button>';
-                        echo  '<ul class="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">';
-                        if ($_SESSION["roleid"] == 1)
-                        {
-                            echo '<li><a class="dropdown-item" href="admin.php">Admin page</a></li>';
-                        }
-                        else if ($_SESSION["roleid"] == 2)
-                        {
-                            echo '<li><a class="dropdown-item" href="employee.php">Employee page</a></li>';
-                        }
-                        echo  '<li><a class="dropdown-item" href="logout.php">Sign Out</a></li>';
-                        echo  '</ul>';
-                        echo  '</div>';
-                        }
-                        ?>                               
-                    </ul>        
-                </div>
+<div class="sidebar">
+            <div class="sidebar-header">
+                <h4><i class="fas fa-cog"></i> Admin Panel</h4>
             </div>
             
-            <div class="w-100">
-                <a class="navbar-brand fw-bold d-flex align-items-center justify-content-center py-3" href="index.php">
-                    <img src="images/logo.png" alt="Logo" width="64" height="64"> 
-                    <span class="ms-2">Santos Optical</span>
+            <div class="sidebar-menu">
+                <a href="#" class="sidebar-item active">
+                    <i class="fas fa-tachometer-alt"></i> Dashboard
+                </a>
+                
+                <a href="customerRecords.php" class="sidebar-item">
+                    <i class="fas fa-users"></i> Customer Information
+                </a>
+                
+                <a href="EmployeeRecords.php" class="sidebar-item">
+                    <i class="fas fa-user-tie"></i> Manage Employees
+                </a>
+                
+                <a href="admin-inventory.php" class="sidebar-item">
+                    <i class="fas fa-boxes"></i> Manage Inventory
+                </a>
+                
+                <a href="#" class="sidebar-item">
+                    <i class="fas fa-shopping-cart"></i> Orders
+                </a>
+                
+                <a href="#" class="sidebar-item">
+                    <i class="fas fa-box-open"></i> Products
+                </a>
+                
+                <a href="#" class="sidebar-item">
+                    <i class="fas fa-layer-group"></i> Collections
+                </a>
+                
+                <a href="#" class="sidebar-item">
+                    <i class="fas fa-archive"></i> Package
                 </a>
             </div>
-        </div>                   
-    </nav>
-</div>
-
-<style>
-    .forNavigationbar {
-        width: 250px;
-        height: 100vh;
-        position: fixed;
-        left: 0;
-        top: 0;
-    }
-    
-    .logo {
-        width: 30px;
-        height: 30px;
-        border-radius: 50%;
-        margin-left: 10px;
-    }
-    
-    .navbar {
-        height: 100%;
-    }
-    
-    .dropdown-menu {
-        position: relative !important;
-    }
-    
-    .container-fluid {
-        min-height: 100%;
-    }
-</style>
+        </div>
