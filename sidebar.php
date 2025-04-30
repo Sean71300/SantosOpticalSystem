020dbd8beebd4edb47ae1e80b2360a52098d1ebf
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-05-01 01:45:35 +0800

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -51,8 +51,8 @@ if ($isAdmin === true) {
             <i class="fas fa-boxes"></i> Manage Inventory
         </a>
         
-        <a href="order.php" class="sidebar-item">
-            <i class="fas fa-shopping-cart"></i> Orders
+        <a href="order.php" class="sidebar-item <?php echo ($current_page == 'order.php') ? 'active' : ''; ?>">
+            <i class="fas fa-shopping-cart"></i> Manage Orders
         </a>
         
         <!-- Logout Button -->
