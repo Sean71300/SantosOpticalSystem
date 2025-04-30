7cb1692eeb879675f52614ea57f8793b84191b49
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-05-01 07:01:17 +0800

    Update admin-inventory-funcs.php

--- admin-inventory-funcs.php
+++ admin-inventory-funcs.php
@@ -83,7 +83,7 @@ function getInventory($sort = 'ProductID', $order = 'ASC') {
     
     $branchName = $_SESSION['current_branch'] ?? '';
     
-    $validSortColumns = ['ProductID', 'CategoryType', 'ShapeDescription', 'BrandName', 'Model', 'TotalCount', 'LastUpdated', 'Count', 'Upd_dt'];
+    $validSortColumns = ['ProductID', 'CategoryType', 'ShapeDescription', 'BrandName', 'Model', 'TotalCount', 'LastUpdated', 'Stocks', 'Upd_dt'];
     $sort = in_array($sort, $validSortColumns) ? $sort : 'ProductID';
     $order = strtoupper($order) === 'DESC' ? 'DESC' : 'ASC';
     
@@ -138,7 +138,7 @@ function getInventory($sort = 'ProductID', $order = 'ASC') {
                     <td class='align-middle'>".htmlspecialchars($row['Model'])."</td>
                     <td class='align-middle'>".htmlspecialchars($row['Material'])."</td>
                     <td class='align-middle'><img src='".htmlspecialchars($row['ProductImage'])."' class='product-img'></td>
-                    <td class='align-middle'>".htmlspecialchars($row['TotalCount'])."</td>                        
+                    <td class='align-middle'>".htmlspecialchars($row['Available Stocks'])."</td>                        
                 </tr>";
         }
 
@@ -158,7 +158,7 @@ function getInventory($sort = 'ProductID', $order = 'ASC') {
             case 'ShapeDescription': $sql .= " ORDER BY sm.Description"; break;
             case 'BrandName': $sql .= " ORDER BY bm.BrandName"; break;
             case 'Model': $sql .= " ORDER BY pm.Model"; break;
-            case 'Count': $sql .= " ORDER BY pbm.Stocks"; break;
+            case 'Stocks': $sql .= " ORDER BY pbm.Stocks"; break;
             case 'Upd_dt': $sql .= " ORDER BY pbm.Upd_dt"; break;
             default: $sql .= " ORDER BY pm.ProductID";
         }
@@ -339,7 +339,7 @@ function addProduct(){ //Add function to add a new product to the database
             mysqli_stmt_close($stmt);
             
             // Insert product-branch mapping into the product branch master database
-            $sql = "INSERT INTO ProductBranchMaster (ProductBranchID, ProductID, BranchCode, Count, Avail_FL, Upd_by, Upd_dt)
+            $sql = "INSERT INTO ProductBranchMaster (ProductBranchID, ProductID, BranchCode, Stocks, Avail_FL, Upd_by, Upd_dt)
                     VALUES (?, ?, ?, ?, ?, ?, ?)"; 
             $stmt = mysqli_prepare($link, $sql);
             mysqli_stmt_bind_param($stmt, "sssisds", $newProductBranchID, $newProductID, $newProductBranchCode, $newProductQty, $avail_FL, $upd_by, $upd_dt);
@@ -508,7 +508,7 @@ function editProduct(){ //Edit function to edit an existing product in the datab
                                 <input type="text" class="form-control" id="price" name="price" value="' . htmlspecialchars($price) . '" required>
                             </div>
                             <div class="mb-3">
-                                <label for="count" class="form-label">Count</label>
+                                <label for="count" class="form-label">Stocks</label>
                                 <input type="number" class="form-control" id="count" name="count" value="' . htmlspecialchars($count) . '" min="0" required>
                             </div>
                             <div class="mb-3">
@@ -672,7 +672,7 @@ function confirmEditProduct() {
         mysqli_stmt_close($stmt1);
 
         // Update productbranchmaster table
-        $sql2 = "UPDATE ProductBranchMaster SET ProductBranchID = ?, BranchCode = ?, Count = ?, Avail_FL = ?, Upd_by = ?, Upd_dt = ? WHERE ProductID = ?";
+        $sql2 = "UPDATE ProductBranchMaster SET ProductBranchID = ?, BranchCode = ?, Stocks = ?, Avail_FL = ?, Upd_by = ?, Upd_dt = ? WHERE ProductID = ?";
         $stmt2 = mysqli_prepare($link, $sql2);
         mysqli_stmt_bind_param($stmt2, "sssssss", $productBranchID, $branchCode, $count, $avail_FL, $employeeName, $upd_dt, $productID);
         if (!mysqli_stmt_execute($stmt2)) {
