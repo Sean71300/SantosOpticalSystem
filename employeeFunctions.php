6aa9f9d918d603996fc3a21ad6daec5b5900cbcb
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-05-01 17:43:51 +0800

    Update employeeFunctions.php

--- employeeFunctions.php
+++ employeeFunctions.php
@@ -223,7 +223,7 @@
                 $stmt = $conn->prepare("INSERT INTO Logs 
                                     (LogsID, EmployeeID, TargetID, TargetType, ActivityCode, Description, Upd_dt)
                                     VALUES
-                                    (?, ?, ?, 'customer', '3', ?, NOW())");
+                                    (?, ?, ?, 'employee', '3', ?, NOW())");
                 $stmt->bind_param("ssss", $Logsid, $employee_id, $id, $name);
                 $stmt->execute();
                 $stmt->close();
