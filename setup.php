c527a78960e53cd5479d657d5e36846b4f467fde
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-05-01 21:08:27 +0800

    Update setup.php

--- setup.php
+++ setup.php
@@ -1162,7 +1162,7 @@
                 
                 FOREIGN KEY (CustomerID) REFERENCES customer(CustomerID) ON DELETE CASCADE,
                 INDEX (CustomerID),
-                INDEX (visit_date)
+                INDEX (visit_date),
                 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
         
         if (mysqli_query($conn, $sql)) {
