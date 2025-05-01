7962e764daf6b7c04f02fbe3664c900afb4f1709
esistlex <estislex@gmail.com>
2025-05-01 13:44:49 +0800

    gallery progress

--- product-gallery.php
+++ product-gallery.php
@@ -17,7 +17,7 @@
         $totalPages = ceil($total / $perPage);
 
         while($row = mysqli_fetch_assoc($result)) {
-            echo "<div class='col g-3 align-self-center'>";
+            echo "<div class='col g-3'>";
                     echo "<div class='card' style='width: 18.5rem; height: 29.5rem;'>";
                         echo '<img src="' . $row['ProductImage']. '"class="card-img-top" style="height: 250px;" alt="'. $row['Model'] .'">';
                             echo "<div class='card-body'>";
@@ -87,7 +87,7 @@
             </div>
 
             <div class="grid" style="margin-bottom: 3.5rem;">
-                <div class="row justify-content-evenly">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
                     <?php
                         pagination();
                     ?>
