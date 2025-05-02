39f353cc3339f36214f08c498a3f8163325b624d
esistlex <estislex@gmail.com>
2025-05-02 17:55:48 +0800

    Remove unused Bootstrap JavaScript bundle from Employee inventory page

--- Employee-inventory.php
+++ Employee-inventory.php
@@ -44,7 +44,6 @@ if ($link) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
         <link rel="stylesheet" href="customCodes/custom.css">
         <link rel="shortcut icon" type="image/x-icon" href="Images/logo.png"/>
