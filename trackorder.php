7b604ec0c49e36697e70a2d07938d3a79eb564d5
esistlex <estislex@gmail.com>
2025-05-02 19:10:07 +0800

    track-order prep

--- trackorder.php
+++ trackorder.php
@@ -1,4 +1,5 @@
 <?php
+include_once 'setup.php';
 
 ?>
 
@@ -24,8 +25,7 @@
 </header>
 
 <body>
-
-    <div class="container h-75">
+    <div class="container" style="height: 20rem;">
         <div class="row justify-content-center mt-5 mb-5">
             <div class="col-md-8">
                 <i class="fa-solid fa-file-invoice-dollar me-2"></i><h1 class="text-center">Track Your Order</h1>
