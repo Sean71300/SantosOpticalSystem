e2fffcb857a1889bded854c13388f13707d650f1
esistlex <estislex@gmail.com>
2025-05-03 00:34:12 +0800

    Fix HTML structure and improve CSS link organization in customer dashboard

--- customer_dashboard.php
+++ customer_dashboard.php
@@ -10,11 +10,14 @@ include 'ActivityTracker.php';
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Customer Dashboard</title>
-        <link rel="shortcut icon" type="image/x-icon" href="Images/logo.png"/>
+        <link rel="shortcut icon" type="image/x-icon" href="Images/logo.png"/>       
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+        <link rel="stylesheet" href="customCodes/custom.css">
+        <link rel="stylesheet" href="customCodes/s1.css">
+        <link rel="stylesheet" href="customCodes/custom.css">
     </head>
 
     <header>
@@ -67,6 +70,6 @@ include 'ActivityTracker.php';
                 </div>
             </div>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
