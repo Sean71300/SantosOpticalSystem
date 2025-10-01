888c2e2376f8330d7f5719c729162a11702b78c5
Alexander Ting <estislex@gmail.com>
2025-10-01 17:59:50 +0800

    branch delete actions

--- admin-branch-funcs.php
+++ admin-branch-funcs.php
@@ -7,9 +7,11 @@ include 'setup.php';
         if (mysqli_num_rows($result) > 0) {
             while ($row = mysqli_fetch_assoc($result)) {
                 echo "<tr>";
-                echo "<td>" . htmlspecialchars($row['BranchName']) . "</td>";
-                echo "<td>" . htmlspecialchars($row['BranchLocation']) . "</td>";
-                echo "<td>" . htmlspecialchars($row['ContactNo']) . "</td>";
+                    echo "<td>" . htmlspecialchars($row['BranchName']) . "</td>";
+                    echo "<td>" . htmlspecialchars($row['BranchLocation']) . "</td>";
+                    echo "<td>" . htmlspecialchars($row['ContactNo']) . "</td>";
+                    echo "<td><a href='admin-edit-branch.php?branch=" . urlencode($row['BranchName']) . "' class='btn btn-primary btn-sm'>Edit</a></td>";
+                    echo "<td><a href='admin-delete-branch.php?branch=" . urlencode($row['BranchName']) . "' class='btn btn-danger btn-sm' onclick='return confirm(\"Are you sure you want to delete this branch?\")'>Delete</a></td>";
                 echo "</tr>";
             }
         } else {
