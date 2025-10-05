970310f3a851b7dd50ce464da7a7ac3ae841ee7c
Alexander Ting <estislex@gmail.com>
2025-10-05 13:57:25 +0800

    Add delete branch modal with confirmation prompt in admin-branch-funcs.php

--- admin-branch-funcs.php
+++ admin-branch-funcs.php
@@ -80,6 +80,26 @@ include 'setup.php';
     }
 
     function deleteBranch() {
-
+        echo 
+            '<div class="modal fade" id="deleteBranchModal" aria-labelledby="deleteBranchModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="deleteBranchModalLabel">Delete Branch</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Are you sure you want to delete this branch?</p>
+                            <form method="post">
+                                <input type="hidden" name="branchCode" value="'.htmlspecialchars($_POST['branchCode'] ?? '').'">
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-danger w-25" name="confirmDeleteBtn">Delete</button>
+                                    <button type="button" class="btn btn-secondary w-25" data-bs-dismiss="modal">Cancel</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>';
     }
 ?>
