a65f660a3b93d3d4dfb3df9708b9b709663c19d8
Sean71300 <145242096+Sean71300@users.noreply.github.com>
2025-10-06 01:46:43 +0800

    Update order.php

--- order.php
+++ order.php
@@ -339,6 +339,7 @@ if ($ordersCount === 0 && !empty($headers)) {
 <meta name="viewport" content="width=device-width,initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
+<link rel="shortcut icon" type="image/x-icon" href="Images/logo.png"/>
 <title>Orders Management</title>
 <style>
 body { background:#f5f7fa; padding-top:60px; }
