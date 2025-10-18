5303a4b105aab1941b2bb887ecda9659484d36dd
Alexander Ting <estislex@gmail.com>
2025-10-18 20:48:36 +0800

    Update Google Maps API key placeholder and consolidate CSS styles in map.php

--- map.php
+++ map.php
@@ -3,9 +3,23 @@
   <head>
     <title>Simple Marker</title>
     <!-- The callback parameter is required, so we use console.debug as a noop -->
-    <script async src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBfPI5kUaCUugAlg9iU0I-fhkOrqKqRtUA&callback=console.debug&libraries=maps,marker&v=beta">
+    <script async src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY_HERE&callback=console.debug&libraries=maps,marker&v=beta">
     </script>
-    <link rel="stylesheet" href="./style.css"/>
+    <style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      gmp-map {
+        height: 100%;
+      }
+
+      /* Optional: Makes the sample page fill the window. */
+      html,
+      body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+      }
+    </style>
   </head>
   <body>
     <gmp-map center="40.12150192260742,-100.45039367675781" zoom="4" map-id="DEMO_MAP_ID">
